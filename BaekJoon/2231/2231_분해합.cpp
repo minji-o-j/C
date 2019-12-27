633c3aba7db2554621d5754fb59b7e3cf23f9e54
Minji <jminji98@gmail.com>
2019-12-28 01:36:16 +0900

    분해합

---
+++ BaekJoon/2231/2231_분해합.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(void)
+{
+	int n, icpy;
+	scanf_s("%d", &n);
+	int minCon=n;// Constructure
+	int state = -1;
+
+	for (int i = n; i > 0; i--)
+	{
+		int sum = 0;
+		int icpy = i;
+
+
+		do {
+			sum += icpy % 10;
+			icpy /= 10;
+		} while (icpy > 0);
+
+		if (sum + i == n)
+		{
+			if (minCon > i)
+			{
+				minCon = i;
+				state = 1;
+			}
+		}
+	}
+	if (state == 1)
+		printf("%d", minCon);
+	else
+		printf("0");
+
+}
