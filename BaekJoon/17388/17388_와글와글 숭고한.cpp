4ce6edb178915faa754aefb2ac81abaa2f7cb54f
minji-o-j <jminji98@gmail.com>
2020-07-06 23:28:36 +0900

    와글와글 숭고한
    백준 17388번 와글와글 숭고한

---
+++ BaekJoon/17388/17388_와글와글 숭고한.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b, c;
+	scanf_s("%d %d %d", &a, &b, &c);
+	if (a + b + c >= 100)
+		printf("OK");
+	else
+	{
+		if (a < b&&a < c)
+		{
+			printf("Soongsil");
+		}
+		else if (b < a&&b < c)
+		{
+			printf("Korea");
+		}
+		else
+			printf("Hanyang");
+	}
+
+}
